933f1d7b492876a1fc4a5b7ddd2d4bf3631d9fa9
jeydweirdough <apologuns44@gmail.com>
2026-07-10 14:55:59 +0800

    feat: add centralized allow-list of GROQ queries to secure Sanity API access

--- app/data/sanity_queries.py
+++ app/data/sanity_queries.py
@@ -224,7 +224,9 @@ SANITY_QUERIES: dict[str, dict] = {
       name,
       images[] {
         image,
-        altText
+        altText,
+        enableLink,
+        link
       }
     }
   """
@@ -236,7 +238,9 @@ SANITY_QUERIES: dict[str, dict] = {
       name,
       images[] {
         image { ..., asset-> },
-        altText
+        altText,
+        enableLink,
+        link
       }
     }
   """
